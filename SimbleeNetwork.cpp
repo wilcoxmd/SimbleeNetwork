9eff0c3cc7c56ec1c269a6403ea3f2810b8dc06e
Matt Wilcox <mwilcox@lunalights.org>
2016-03-31 18:06:10 -0500

    refined network sending functions to send to specific addresses and read data

--- SimbleeNetwork.cpp
+++ SimbleeNetwork.cpp
@@ -1,24 +1,24 @@
 #include "SimbleeNetwork.h"
 #include "SimbleeCOM.h"
 
-#define TARGET_ID 0xD35EF111
-//#define TARGET_ID 0xD35EF11E 	//the real id:
-SimbleeNetwork::SimbleeNetwork(int pin)
+//#define TARGET_ID 0xD35EF11E
+//#define TARGET_ID 0xD35EF11E 	//the real id
+
+SimbleeNetwork::SimbleeNetwork()
 {
-	pinMode(pin, OUTPUT);
+
+}	
+
+void SimbleeNetwork::begin()
+{
+	SimbleeCOM.begin();
+
 	myESN = SimbleeCOM.getESN();
+
 	SENDER_ID_0 = myESN & 0x000000FF;
 	SENDER_ID_1 = (myESN & 0X0000FF00) >> 8;
 	SENDER_ID_2 = (myESN & 0X00FF0000) >> 16;
 	SENDER_ID_3 = (myESN & 0XFF000000) >> 24;
-
-
-}	
-
-
-void SimbleeNetwork::begin()
-{
-
 }
 
 void SimbleeNetwork::join()
@@ -26,27 +26,84 @@ void SimbleeNetwork::join()
 
 }
 
+void SimbleeNetwork::setAsMaster()
+{
+	groupESNs[0] = myESN;
+
+	GROUP_ID_0 = myESN & 0x000000FF;
+	GROUP_ID_1 = (myESN & 0X0000FF00) >> 8;
+	GROUP_ID_2 = (myESN & 0X00FF0000) >> 16;
+	GROUP_ID_3 = (myESN & 0XFF000000) >> 24;
+
+}
+
 int SimbleeNetwork::send(uint16_t data, uint32_t targetAddress)
 {
-	char TARGET_ID_0 = TARGET_ID & 0x000000FF;
-	char TARGET_ID_1 = (TARGET_ID & 0X0000FF00) >> 8;
-	char TARGET_ID_2 = (TARGET_ID & 0X00FF0000) >> 16;
-	char TARGET_ID_3 = (TARGET_ID & 0XFF000000) >> 24;
+	char TARGET_ID_0 = targetAddress & 0x000000FF;
+	char TARGET_ID_1 = (targetAddress & 0X0000FF00) >> 8;
+	char TARGET_ID_2 = (targetAddress & 0X00FF0000) >> 16;
+	char TARGET_ID_3 = (targetAddress & 0XFF000000) >> 24;
 
-	char payload[] = {SENDER_ID_0, SENDER_ID_1, SENDER_ID_2, SENDER_ID_3, TARGET_ID_0, TARGET_ID_1, TARGET_ID_2, TARGET_ID_3};
+	//Place holder for now. can create diffrent data types later
+	char DATA_TYPE = 0;
+
+	char DATA_BYTE_0 = data & 0x00FF;
+	char DATA_BYTE_1 = (data & 0xFF00) >> 8;
+
+	//construct payload packet
+	char payload[] = {GROUP_ID_0, GROUP_ID_1, GROUP_ID_2, GROUP_ID_3, SENDER_ID_0, SENDER_ID_1, SENDER_ID_2, SENDER_ID_3, TARGET_ID_0, TARGET_ID_1, TARGET_ID_2, TARGET_ID_3, DATA_TYPE, DATA_BYTE_0, DATA_BYTE_1};
+	
+	//send payload packet over simbleeCOM
 	SimbleeCOM.send(payload, sizeof(payload));
-	return 100;
+
+
 }
+
+bool SimbleeNetwork::sendACK(uint32_t targetAddress)
+{
+
+	char TARGET_ID_0 = targetAddress & 0x000000FF;
+	char TARGET_ID_1 = (targetAddress & 0X0000FF00) >> 8;
+	char TARGET_ID_2 = (targetAddress & 0X00FF0000) >> 16;
+	char TARGET_ID_3 = (targetAddress & 0XFF000000) >> 24;
+
+	//send ACK type message
+	char DATA_TYPE = ACK;
+	char DATA_BYTE_0 = 0;
+	char DATA_BYTE_1 = 0;
+
+	//construct ACK payload packet
+	char payload[] = {GROUP_ID_0, GROUP_ID_1, GROUP_ID_2, GROUP_ID_3, SENDER_ID_0, SENDER_ID_1, SENDER_ID_2, SENDER_ID_3, TARGET_ID_0, TARGET_ID_1, TARGET_ID_2, TARGET_ID_3, DATA_TYPE, DATA_BYTE_0, DATA_BYTE_1};
+
+	SimbleeCOM.send(payload, sizeof(payload));
+}
+
 
 bool SimbleeNetwork::checkAddress(const char* payload)
 {
+	//reconstruct target address from payload
 	uint32_t target_address = 0;
-	target_address |= (payload[7] << 24);
-	target_address |= (payload[6] << 16);
-	target_address |= (payload[5] << 8);
-	target_address |= (payload[4] 	  );
+	target_address |= (payload[11] << 24);
+	target_address |= (payload[10] << 16);
+	target_address |= (payload[9] << 8);
+	target_address |= (payload[8] 	  );
 
-	if (target_address == myESN)
+	uint32_t sender_id = 0;
+	sender_id |= (payload[7] << 24);
+	sender_id |= (payload[6] << 16);
+	sender_id |= (payload[5] << 8);
+	sender_id |= (payload[4] 	  );
+
+	uint8_t data_type = readDataType(payload);
+
+	//check for our address or a braodcast message (address == 0)
+	if ((target_address == myESN || target_address == 0) && (data_type != ACK))
+	{	
+		//send ACK here
+		sendACK(sender_id);
+		return 1;
+	}
+	else if (target_address == myESN && data_type == ACK) 
 	{
 		return 1;
 	}
@@ -54,9 +111,52 @@ bool SimbleeNetwork::checkAddress(const char* payload)
 	{
 		return 0;
 	}
-
-
 }
+
+uint8_t SimbleeNetwork::readDataType(const char* payload)
+{
+	//reconstruct DATA_TYPE from packet
+	uint8_t dataType = 0;
+	dataType |= (payload[12] 	 );
+
+	return dataType; 
+}
+
+uint16_t SimbleeNetwork::readData(const char* payload)
+{
+	//reconstruct data from packet
+	uint16_t data = 0;
+	data |= (payload[14] << 8);
+	data |= (payload[13] 	 );
+
+	return data; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- SimbleeNetwork.h
+++ SimbleeNetwork.h
@@ -5,20 +5,25 @@
 //  Byte #		|	  Purpose
 //--------------------------------
 //		0			  GROUP_ID_0		//GROUP_ID is defined by the 
-//		1			  GROUP_ID_1		//ESN of the pressure sensor in each group
+//		1			  GROUP_ID_1		//ESN of the master node in each group
 //		2			  GROUP_ID_2		
 //		3			  GROUP_ID_3	
 //		4			  SENDER_ID_0
 //		5			  SENDER_ID_1		//SENDER_ID is defined by 
-//		6			  SENDER_ID_2		//the ESN of the light or sensor sending the message
+//		6			  SENDER_ID_2		//the ESN of the node sending the message
 //		7			  SENDER_ID_3
 //		8			  TARGET_ID_0
 //		9			  TARGET_ID_1		//TARGET_ID is defined by 
 //		10			  TARGET_ID_2		//the ESN of the intended message receipient
 //		11			  TARGET_ID_3	
-//		12			  REQUEST_TYPE		//REQUEST_TYPE defines what 
-//		13			  REQUEST_DATA_0	type of message we will be handling
-//		14			  REQUEST_DATA_1	//REQUEST_DATA contains the packet data to be interepreted by the recipient
+//		12			  DATA_TYPE		//DATA_TYPE defines what 
+//		13			  DATA_BYTE_0	type of message we will be handling
+//		14			  DATA_BYTE_1	//DATA_BYTE contains the packet data to be interepreted by the recipient
+
+//TO DO: 
+//1. test for larger packet sizes to hold more data. Currently based off of other 
+//		Simblee examples, which use a 15 total byte packet		
+
 
 #include "Arduino.h"
 
@@ -28,7 +33,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define MAX_PACKET_DATA_SIZE 2  //max number of bytes of packet data
+//DATA_TYPE definitions
+#define ACK 	1
+
 
 
 class SimbleeNetwork
@@ -36,10 +43,9 @@ class SimbleeNetwork
 	public: 
 
 	//Constructor
-	SimbleeNetwork(int pin);
+	SimbleeNetwork();
 
-	//a function to begin a new network based on a sensor radio's ESN
-	//NOTE: for use by controller radios only
+	//starts SimbleeCOM library and establishes the ESN and SENDER_ID properties of this radio
 	void begin();
 
 	//a function to join an existing network
@@ -50,15 +56,33 @@ class SimbleeNetwork
 
 
 	//function to send data to a specific target address
-	//set target address to 0 to broadcast to all radios in the group
+	//set target address to 0 to broadcast to all radios in the 
+	//TO DO: return a bool success message to ensure message was transmitted
 	int send(uint16_t data, uint32_t targetAddress);
 
 	//function to check for a target address match to our own address
 	bool checkAddress(const char* payload);
 
+	//function to determine DATA_TYPE from payload
+	uint8_t readDataType(const char* payload);
+
+	//function to read in received data
+	uint16_t readData(const char* payload);
+
+	//establish this radio as a master node. must call this function to start communications
+	//for a group. Only one master node allowed per group. Master nodes
+	//are responsible for managing known addresses within a group
+	void setAsMaster();
+
+	//function to send an ACK back to a node to acknowledge packet transmission
+	bool sendACK(uint32_t targetAddress);
+
 	//variable to store this radio's ESN (ID)
 	uint32_t myESN;
 
+	//array stores all the ESNs that belong to a group. groupESNs[0] is reserved for the 
+	//ESN of the master node and establishes the GROUP_ID
+	uint32_t groupESNs[6];
 
 
 	private:
